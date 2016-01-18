ee2d6c9ab0498d1e2f9b5e0df8b0044d91091e17
Kevin Nick <nickevin@gmail.com>
2016-01-18 20:48:19 +0800

    feat: use spring seesion replace tomcat-redis-session-manager

---
+++ src/main/java/com/qihua/config/HttpSessionConfig.java
@@ -0,0 +1,17 @@
+package com.qihua.config;
+
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+/**
+ * Class description goes here.
+ * 
+ * @author aopfilter@163.com
+ * @since Jan 13, 2016
+ * @version 1.0
+ * @see
+ */
+@EnableRedisHttpSession
+public class HttpSessionConfig {
+
+}
+
